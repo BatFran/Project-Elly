0e2cdadd4b377f67190913dfbd681b889b65c34d
BatFran <batfran0621@gmail.com>
2018-07-11 00:21:47 -0400

    Initial commit

---
+++ read_xhtml.py
@@ -0,0 +1,12 @@
+#will import Russian text, and then output in editor
+import os
+from bs4 import BeautifulSoup
+print("cwd: ", os.getcwd())
+
+print ("hello world")
+
+soup = BeautifulSoup(open(".\dnevnik_1890_51_tom(1)/OEBPS/Text/0001_1009_2001.xhtml", encoding = 'utf-8'), 'html.parser')
+
+#print(soup.prettify())
+
+print(soup.title)
